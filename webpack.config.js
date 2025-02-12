b82e1977a91b7f483c38456e79f20e8d506df4eb
ibimina hart <ibimina.c.hart@gmail.com>
2025-02-12 08:42:03 +0100

    monorepo microfrontend

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  devServer: {
+    port: 3001,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, // or /\.(ts|tsx)$/ if using TypeScript
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+            {
+                test: /\.svg$/,
+                oneOf: [
+                    {
+                        resourceQuery: /component/, // Use "?component" in import to load as React component
+                        use: ["@svgr/webpack"],
+                    },
+                    {
+                        use: {
+                            loader: "file-loader",
+                            options: {
+                                name: "[name].[hash].[ext]",
+                                outputPath: "assets", // Save inside "dist/assets/"
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },    
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "microfrontend",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MicroApp": "./src/App",  // Exposing the Microfrontend's App component
+      },
+      shared: {
+        react: { singleton: true, eager: true },
+        'react-dom': { singleton: true, eager: true },
+      },    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
